9dd7a3db2a65662b9877748db01e55542204f892
Nel <128928886+Akanel0007@users.noreply.github.com>
2023-06-11 12:59:50 +0300

    Add files via upload

---
+++ NelRulesUI/NelRulesUI/Main.cs
@@ -0,0 +1,79 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NelRulesUI
+{
+    public class Main : RocketPlugin<NelRulesConfig>
+    {
+        public static Main Instance;
+
+        protected override void Load()
+        {
+            U.Events.OnPlayerConnected += Events_OnPlayerConnected;
+            U.Events.OnPlayerDisconnected += Events_OnPlayerDisconnected;
+            EffectManager.onEffectButtonClicked += OnButtonClicked;
+            Logger.Log("Akanel#0007");
+            Logger.Log("Support Discord");
+            Logger.Log("https://discord.gg/2D9VNwcQEB");
+            Logger.Log("https://discord.gg/2D9VNwcQEB");
+            Logger.Log("https://discord.gg/2D9VNwcQEB");
+            Logger.Log("Contact Instagram @taner.yumukk Or Discord Akanel#0007");
+            Logger.Log("Rules UI Loaded");
+            Logger.Log("Akanel#0007");
+        }
+
+        private void Events_OnPlayerDisconnected(Rocket.Unturned.Player.UnturnedPlayer player)
+        {
+            EffectManager.askEffectClearByID(7525, player.SteamPlayer().transportConnection);
+        }
+
+        private void Events_OnPlayerConnected(Rocket.Unturned.Player.UnturnedPlayer player)
+        {
+            EffectManager.sendUIEffect(7525, 7525, player.SteamPlayer().transportConnection, false);
+            EffectManager.sendUIEffectText(7525, player.Player.channel.owner.transportConnection, true, "KuralbirText", Configuration.Instance.Message1);
+            EffectManager.sendUIEffectText(7525, player.Player.channel.owner.transportConnection, true, "KuralikiText", Configuration.Instance.Message2);
+            EffectManager.sendUIEffectText(7525, player.Player.channel.owner.transportConnection, true, "KuralucText", Configuration.Instance.Message3);
+            EffectManager.sendUIEffectText(7525, player.Player.channel.owner.transportConnection, true, "KuraldortText", Configuration.Instance.Message4);
+            EffectManager.sendUIEffectText(7525, player.Player.channel.owner.transportConnection, true, "KuralbesText", Configuration.Instance.Message5);
+            EffectManager.sendUIEffectText(7525, player.Player.channel.owner.transportConnection, true, "KuralaltiText", Configuration.Instance.Message6);
+            EffectManager.sendUIEffectText(7525, player.Player.channel.owner.transportConnection, true, "KuralyediText", Configuration.Instance.Message7);
+            EffectManager.sendUIEffectText(7525, player.Player.channel.owner.transportConnection, true, "DiscordText", Configuration.Instance.DiscordNel);
+            EffectManager.sendUIEffectText(7525, player.Player.channel.owner.transportConnection, true, "BaslikText", Configuration.Instance.BaslikNel);
+            EffectManager.sendUIEffectText(7525, player.Player.channel.owner.transportConnection, true, "OnaylaText", Configuration.Instance.ButtonText);
+            EffectManager.sendUIEffectText(7525, player.Player.channel.owner.transportConnection, true, "ReddedText", Configuration.Instance.ReddedText);
+
+        }
+
+        public void OnButtonClicked(Player caller, string buttonName)
+        {
+            var config = Configuration.Instance;
+            UnturnedPlayer player = UnturnedPlayer.FromPlayer(caller);
+
+            if (buttonName == "OnaylaButton")
+            {
+                EffectManager.askEffectClearByID(7525, player.SteamPlayer().transportConnection);
+            }
+            if (buttonName == "ReddedButton")
+            {
+                EffectManager.askEffectClearByID(7525, player.SteamPlayer().transportConnection);
+                UnturnedPlayer unturnedplayer2 = UnturnedPlayer.FromCSteamID(player.CSteamID);
+                unturnedplayer2.Kick("You were kicked from the server for not accepting the rules");
+            }
+        }
+
+        protected override void Unload()
+        {
+            U.Events.OnPlayerDisconnected -= Events_OnPlayerDisconnected;
+            U.Events.OnPlayerConnected -= Events_OnPlayerConnected;
+            EffectManager.onEffectButtonClicked -= OnButtonClicked;
+        }
+    }
+}
---
+++ NelRulesUI/NelRulesUI/NelRulesConfig.cs
@@ -0,0 +1,42 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NelRulesUI
+{
+    public class NelRulesConfig : IRocketPluginConfiguration
+    {
+        public string Message1 { get; set; }
+        public string Message2 { get; set; }
+        public string Message3 { get; set; }
+        public string Message4 { get; set; }
+        public string Message5 { get; set; }
+        public string Message6 { get; set; }
+        public string Message7 { get; set; }
+        public string ButtonText { get; set; }
+        public string DiscordNel { get; set; }
+        public string BaslikNel { get; set; }
+        public string ReddedText { get; set; }
+        public string NelRulesPermission { get; set; }
+
+
+        public void LoadDefaults()
+        {
+            BaslikNel = "ServerName";
+            Message1 = "Nel Plugins Rules";
+            Message2 = "Rule 1";
+            Message3 = "Rule 2";
+            Message4 = "Rule 3";
+            Message5 = "Rule 4";
+            Message6 = "Rule 5";
+            Message7 = "Instagram taner.yumukk / Discord Support Server https://discord.gg/2D9VNwcQEB";
+            DiscordNel = ".gg/codegeassroleplay";
+            ButtonText = "Accept";
+            ReddedText = "Refuse";
+            NelRulesPermission = "Nel.Rules";
+        }
+    }
+}
